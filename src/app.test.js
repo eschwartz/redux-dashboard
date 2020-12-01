c979ffd885296f324d8a095a66776301faec40c2
Edan Schwartz <edanschwartz@gmail.com>
2020-12-01 16:52:43 -0600

    test stubs from rubric

---
+++ src/app.test.js
@@ -0,0 +1,40 @@
+import App from './components/App/App';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import {default as waitFor} from 'wait-for-expect';
+
+
+it('Components use `connect()` to allow dispatch', async() => {
+});
+
+it('Components map Redux State to props to allow props to render ', async() => {
+});
+
+it('Speed: Reducer Increases and Decreases by 1', async() => {
+});
+
+it('Speed: Buttons click dispatches actions to increase and decrease speed reducer value', async() => {
+});
+
+it('Speed: Reducer does all the math', async() => {
+});
+
+it('Speed: Component renders from Redux', async() => {
+});
+
+it('Passenger: View displays list of passenger names', async() => {
+});
+
+it('Passenger: Button click dispatches action with payload of text input field as new string to passenger reducer', async() => {
+});
+
+it('Passenger: Reducer properly uses spread operator', async() => {
+});
+
+it('Dashboard: Displays current speed reducer speed number', async() => {
+});
+
+it('Dashboard: Displays current passenger count', async() => {
+});
+
+
