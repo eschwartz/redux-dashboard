c59e6a3279738a6d7ed5017b9ba29adef8cde245
Edan Schwartz <edanschwartz@gmail.com>
2020-12-02 11:01:16 -0600

    Test: speed buttons (local)

--- src/app.test.js
+++ src/app.test.js
@@ -31,6 +31,57 @@ it('Components use `connect()` to allow dispatch', async() => {
   ).toBeDefined();
 });
 
+it('Speed: Initial speed shows as `0`', async() => {
+  let speedControl = mountWithStore(SpeedControl);
+
+  expect(
+    speedControl.text(),
+    'Speed Control component should render "SPEED: 0" on initial load',
+  ).toMatch(/SPEED:\s*0/i)
+});
+
+it('Speed: Increase/Decrease buttons should update the speed on the DOM', async() => {
+  let speedControl = mountWithStore(SpeedControl);
+
+  // Find the "Increase Speed Button"
+  let increaseButton = findByText(speedControl, 'Increase Speed');
+  expect(
+    increaseButton.length,
+    'Make sure you have a single button that says "Increase Speed"'
+  ).toBe(1);
+
+  // Find the "Decrease Speed Button"
+  let decreaseButton = findByText(speedControl, 'Decrease Speed');
+  expect(
+    decreaseButton.length,
+    'Make sure you have a single button that says "Decrease Speed"'
+  ).toBe(1);
+
+  // Click the "Increase Speed" button 4x times
+  increaseButton.last().simulate('click');
+  increaseButton.last().simulate('click');
+  increaseButton.last().simulate('click');
+  increaseButton.last().simulate('click');
+
+  // Should render "SPEED: 4"
+  expect(
+    speedControl.text(),
+    'Should render "SPEED: 4" after clicking "Increase Speed" 4x times',
+  ).toMatch(/SPEED:\s*4/i)
+
+  // Click the "Decrease Speed" button 4x times
+  decreaseButton.last().simulate('click');
+  decreaseButton.last().simulate('click');
+
+  // Should render "SPEED: 2"
+  expect(
+    speedControl.text(),
+    'Should render correct speed after clicking the "Decrease Speed" button',
+  ).toMatch(/SPEED:\s*2/i)
+
+
+});
+
 /**
 Tests:
 
@@ -68,4 +119,12 @@ function mountWithStore(Component) {
 
   // Find the actual component
   return provider.find(componentName);
+}
+
+// Helper function, to find an Enzyme element by text
+function findByText (wrapper, text) {
+  return wrapper.findWhere(node => (
+    node.type() &&
+    node.text() === text
+  ));
 }
